11b9a53e23d51475ec9b1526fa8f2f1b618a5ae0
Gregor Peach <gregorpeach@gmail.com>
2019-12-15 15:52:00 -0500

    40% demo checkpoint
    This is what I completed for the 40% demo, preserved "as-is".
    It probably wants some edits before being merged

--- src/client/Components/Pages/Home.js
+++ src/client/Components/Pages/Home.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Icon from '@material-ui/core/Icon';
+
+const styles = theme => ({
+  root: {
+      background: 'black'
+  },
+  logo: {
+    maxWidth: 50,
+  },
+  // This group of buttons will be aligned to the right
+  rightToolbar: {
+    marginLeft: 'auto',
+    marginRight: -12,
+  },
+  menuButton: {
+    marginRight: 16,
+    marginLeft: -12,
+  },
+});
 
 class Home extends React.Component {
   constructor(props) {
@@ -42,24 +63,52 @@ class Home extends React.Component {
       return <div>Loading...</div>;
     }
 
+    const { classes } = this.props;
+
     return (
       <div>
-        <AppBar position="static">
+        <AppBar position="static" color="secondary" className={classes.root}>
           <Toolbar>
-            <IconButton edge="start" color="inherit" onClick={this.updateComponents}>
-              <Icon className="fas fa-sync" />
-            </IconButton>
+            <section className={classes.menuButton}>
+            <img src="/favicon.ico" alt="logo" className={classes.logo} />
+            </section>
+            <Typography variant="h4" edge="end" color="inherit">Welcome to Velocity 9!</Typography>
+            <section className={classes.rightToolbar}>
+                <IconButton edge="end" color="inherit" onClick={this.updateComponents}>
+                  <Icon className="fas fa-sync" />
+                </IconButton>
+            </section>
           </Toolbar>
         </AppBar>
 
         <ul>
-          {items.map(worker => (
-            <li key={worker.worker_id}>{worker.worker_id}</li>
-          ))}
+          {items.map(worker => {
+            let data = JSON.parse(worker.status);
+
+            return <li key={worker.worker_id}>
+              Worker #{worker.worker_id}
+              <ul>
+                <li> CPU Usage: {data["cpu_usage"]} </li>
+                <li> Memory Usage: {data["memory_usage"]} </li>
+                <li> Network Failure Rate: {data["network_usage"]} </li>
+                <li> Components: { "[" + data["active_components"].map( cmp => {
+                    console.log(cmp);
+                    return (cmp["id"]["user"] + "/" + cmp["id"]["repo"]);
+                }).join(",") + "]"}
+                </li>
+              </ul>
+            </li>;
+          })}
         </ul>
       </div>
     );
   }
 }
 
-export default Home;
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+// export default Home;
+export default withStyles(styles)(Home);
