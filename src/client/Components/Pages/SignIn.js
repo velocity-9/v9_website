8a3ce26b0ffd1b7a302d3caab106a64ca823f2d9
Pat Cody <patcody028@gmail.com>
2020-01-01 15:32:23 -0500

    Added basic routing functionality

---
+++ src/client/Components/Pages/SignIn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SignIn extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Temp</p>
+      </div>
+    )
+  }
+}
+
+export default SignIn;
