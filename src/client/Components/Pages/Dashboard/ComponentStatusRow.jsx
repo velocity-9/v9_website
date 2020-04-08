2d9bfc4a04541d245e7f99a140dbdb15eca62024
Pat Cody <patcody028@gmail.com>
2020-04-08 16:06:32 -0400

    Add color bubble to dashboard rows

--- src/client/Components/Pages/Dashboard/ComponentStatusRow.jsx
+++ src/client/Components/Pages/Dashboard/ComponentStatusRow.jsx
@@ -4,6 +4,10 @@ import Button from '@material-ui/core/Button';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
+import blue from '@material-ui/core/colors/blue';
+import green from '@material-ui/core/colors/green';
+import yellow from '@material-ui/core/colors/yellow';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -15,17 +19,23 @@ type ComponentStatusRowProps = {
 
 export default function ComponentStatusRow(props: ComponentStatusRowProps) {
   let componentStatus = 'Pending';
+  let statusColor = { color: blue[600] };
   if (props.component.isDeploying) {
     componentStatus = 'Deploying';
   } else if (props.component.deploymentIntention === 'paused') {
     componentStatus = 'Paused';
+    statusColor = { color: yellow[500] };
   } else if (props.component.deploymentIntention === 'active' && props.component.isRunning) {
     componentStatus = 'Running';
+    statusColor = { color: green[500] };
   }
 
   return (
     <TableRow>
       <TableCell><Link to={`/component/${props.component.username}/${props.component.componentName}`}><Button color="primary">{ props.component.componentName }</Button></Link></TableCell>
+      <TableCell>
+        <FiberManualRecordIcon fontSize="large" style={statusColor} />
+      </TableCell>
       <TableCell><Typography variant="h6">{componentStatus}</Typography></TableCell>
       <TableCell>
         <ActionButton component={props.component} />
