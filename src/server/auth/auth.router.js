d07fbf53f1ef9d1538d923242ee0426f4e41dc56
Pat Cody <patcody028@gmail.com>
2020-04-20 17:36:20 -0400

    Bug fix: Log in error with no cookie

--- src/server/auth/auth.router.js
+++ src/server/auth/auth.router.js
@@ -48,14 +48,9 @@ export default class AuthRouter {
 
     req.session.user = user.name;
     this.database.createNewUser(req.user.username, 'test@test.com');
-    console.log(req);
-    if (!('cookies' in req)) {
-      console.log('Null object was found!');
-      res.redirect('http://v9_website.ngrok.io');
-    } else {
-      res.cookie(req.cookies).redirect('http://v9_website.ngrok.io');
-    }
+    res.redirect('http://v9_website.ngrok.io');
   }
+
 
   getRouter() {
     return this.router;
